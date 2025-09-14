c53cc477e6e46badb1cfcb5d62aa2aa982a4ccd6
Daniel Avina Neri <ld.avinaneri@ugto.mx>
2025-09-14 15:50:51 -0600

    Initial Shiny app structure

---
+++ server.R
@@ -0,0 +1,9 @@
+# server.R
+library(shiny)
+
+# Define la lógica del servidor
+function(input, output, session) {
+  
+  # (Aquí irá toda la magia más adelante)
+  
+}
---
+++ ui.R
@@ -0,0 +1,10 @@
+# ui.R
+library(shiny)
+
+# Define la Interfaz de Usuario para la aplicación
+fluidPage(
+  
+  # Título de la aplicación
+  titlePanel("LDAN Data Analysis Tool")
+  
+)
